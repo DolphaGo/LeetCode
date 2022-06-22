23989521b9fb053e2ebc282b5d426b30126a46b9
DolphaGo <adamdoha@naver.com>
2022-06-23 04:06:44 +0900

    Time: 196 ms (90.18%), Space: 35.7 MB (74.55%) - LeetHub

--- 67-add-binary/67-add-binary.kt
+++ 67-add-binary/67-add-binary.kt
@@ -1,5 +1,18 @@
 class Solution {
     fun addBinary(a: String, b: String): String {
-        return a.toBigInteger(2).add(b.toBigInteger(2)).toString(2)
+        val targetStr = StringBuilder()
+        var ai = a.lastIndex
+        var bi = b.lastIndex
+        var sum = 0
+        while (ai >= 0 || bi >= 0) {
+            if (ai >= 0) sum += a[ai--] - '0'
+            if (bi >= 0) sum += b[bi--] - '0'
+            targetStr.append(sum % 2)
+            sum /= 2
+        }
+        if (sum != 0) {
+            targetStr.append(sum)
+        }
+        return targetStr.toString().reversed()
     }
 }
