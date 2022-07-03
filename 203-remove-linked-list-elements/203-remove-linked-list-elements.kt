986f1d249504a6a58d1240a5893c921712f1266f
DolphaGo <adamdoha@naver.com>
2022-07-04 03:07:52 +0900

    Time: 361 ms (56.91%), Space: 46.3 MB (23.76%) - LeetHub

---
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.kt
@@ -0,0 +1,15 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    fun removeElements(head: ListNode?, `val`: Int): ListNode? {
+        head?.next = removeElements(head?.next, `val`)
+        return if(head?.`val` == `val`) head?.next else head
+    }
+}
